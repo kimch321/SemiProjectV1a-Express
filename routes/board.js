71b3da81b75270b8fe4cad2d7896114269da440c
KIM CHUNG IL <cnddlf12@naver.com>
2023-02-16 11:33:02 +0900

    route 수정, html 파일들 링크 수정

--- routes/board.js
+++ routes/board.js
@@ -2,7 +2,21 @@ const express = require("express");
 const router = express.Router();
 const path = require('path');
 
-
+router.get('/join',(req,res)=> {
+    res.sendFile(path.join(__dirname,'../public','join.html'));
+})
+router.get('/list',(req,res)=> {
+    res.sendFile(path.join(__dirname,'../public','list.html'));
+})
+router.get('/view',(req,res)=> {
+    res.sendFile(path.join(__dirname,'../public','view.html'));
+})
+router.get('/delete',(req,res)=> {
+    res.sendFile(path.join(__dirname,'../public','delete.html'));
+})
+router.get('/update',(req,res)=> {
+    res.sendFile(path.join(__dirname,'../public','update.html'));
+})
 
 
 module.exports = router;
