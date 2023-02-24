5144dbfc1a669b41292d98357ca173f8da4aa21e
KIM CHUNG IL <cnddlf12@naver.com>
2023-02-24 10:48:45 +0900

    게시판 페이징 구현2 - 이전, 다음 버튼 표시

--- routes/board.js
+++ routes/board.js
@@ -39,23 +39,47 @@ router.post('/write',async (req,res)=> {
 // ednum : stnum + (perpage -1)
 router.get('/list',async (req,res)=> {
     let {cpg} =req.query;
-    cpg = cpg ? cpg : 1;
+    cpg = 0 ? 1 : Number(cpg);
+    cpg = cpg ? Number(cpg) : 1;
     let stnum = (cpg - 1) * ppg+1; // 지정한 페이지 범위의 시작값
-    let stpgn = parseInt((cpg-1)/10)*10 + 1; // 페이지네이션 시작값 계산
+
+     // 페이지네이션 시작값 계산
+//     1 페이지의 페이지네이션 : 1 2 3 4 5 6 7 8 9 10
+//     2 페이지의 페이지네이션 : 1 2 3 4 5 6 7 8 9 10
+//     ...
+//     10 페이지의 페이지네이션 : 1 2 3 4 5 6 7 8 9 10
+//
+//     11 페이지의 페이지네이션 : 11 12 13 14 15 16 17 18 19 20
+//     ...
+//     15 페이지의 페이지네이션 : 11 12 13 14 15 16 17 18 19 20
+//     ...
+//     21 페이지의 페이지네이션 : 21 22 23 24 25 26 27 28 29 30
 
     // 페이지네이션 블럭 생성
+    let stpgn = parseInt((cpg-1)/10)*10 + 1;
     let stpgns = [];
+    let allcnt = new Board().selectCnt().then((cnt) => {return cnt}); // 총 게시물 수
+    let alpg = Math.ceil(Number(await allcnt) /ppg)
+
     for(let i = stpgn; i < stpgn+10; ++i) {
-        let iscpg = (i == cpg) ? true: false // 현재페이지 표시
+        if (i <= alpg) { // i가 총 페이지 수보다 같거나 작을때 i 출력
+        let iscpg = (i == cpg) // 현재페이지 표시
         let pgn = {'num' : i, 'iscpg':iscpg}
         stpgns.push(pgn);
+        } else {
+            break;
+        }
     }
-
-    let alpg = new Board().selectCnt().then((cnt) => {return cnt}); // 총 게시물 수
-
-    let isprev = (cpg-1>0) ? true : false;
-    let isnext = (cpg < alpg) ? true : false;
-    let pgn = {'prev' : stpgn - 1, 'next' : stpgn + 9 + 1, 'isprev' : isprev, 'isnext' : isnext};
+    let isprev10 = (cpg - 10 > 0);
+    let isnext10 = (cpg + 10 < alpg);
+    // 단순화한 삼항연산자
+    let isprev = (cpg - 1 > 0);  // 이전 버튼 표시 여부
+    let isnext = (cpg < alpg);  // 다음 버튼 표시 여부
+    let pgn = {'prev' : cpg - 1, 'next' : cpg + 1,
+        'prev10' : cpg - 10 , 'next10' : cpg + 10,
+        'isprev' : isprev, 'isnext' : isnext,
+        'isprev10' : isprev10 , 'isnext10' : isnext10,
+    }; // 이전 : 현재 페이지 -1, 다음 : 현재 페이지 + 1
 
     let board = new Board().select(stnum).then(async (result) => {return result});
 
